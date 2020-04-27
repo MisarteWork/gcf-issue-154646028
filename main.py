5ab1e6fb40380bd4d5dedb66089bfc42c4aa22c1
MisarteWork <58166404+MisarteWork@users.noreply.github.com>
2020-04-27 12:00:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.append(1, '/child-project/child_project_module')
 from flask import Request
-from child-project.child_project_module.__init__ import print_child
+import print_child
 from root_project_module import print_parent
 
 
